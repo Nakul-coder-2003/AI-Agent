0e2c00f735b245091d3c479c87de82624b9e58e6
Nakul Mittal <mittalnakul407@gmail.com>
2026-09-06 00:00:35 +0530

    fix some error

--- backend/services/agent/index.js
+++ backend/services/agent/index.js
@@ -2,7 +2,6 @@ import crypto from "crypto";
 globalThis.crypto = crypto;
 import express from "express"
 import dotenv from "dotenv"
-import cors from "cors"
 import agentRoute from "./routes/agent.route.js";
 
 const app = express();
@@ -10,7 +9,6 @@ dotenv.config();
 
 //middleware
 app.use(express.json());
-app.use(cors());
 
 app.get('/api/agent', (req, res) => {
     res.status(200).json({ 
