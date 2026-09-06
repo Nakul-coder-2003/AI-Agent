d7fa554947cec7963f04f5172bcf4749921d336f
Nakul Mittal <mittalnakul407@gmail.com>
2026-09-06 12:27:54 +0530

    fix webcrypto

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,5 +1,5 @@
 import crypto from "crypto";
-globalThis.crypto = crypto;
+globalThis.crypto = crypto.webcrypto;
 import express from "express"
 import dotenv from "dotenv"
 import ConnectDB from "./src/config/db.js";
